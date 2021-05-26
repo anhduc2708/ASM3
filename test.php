43a022cbc82fa4c642bdc4775d571cce9a8ab9c9
Le Anh Tuan <s3836290@rmit.edu.vn>
2021-05-26 14:47:08 +0700

    test function for php002

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-  $filename = 'install.php';
+  $filename = 'instal1l.php';
   if (file_exists($filename)) {
       echo "<h1> 404 Error Connection </h1>";
       exit(test.php);
@@ -34,6 +34,10 @@
       return $products;
     }
 
+    // read_all_products();
+    // print_r(read_all_products());
+
+
     function get_product($product_id) {
       $products = read_all_products();
       foreach ($products as $p) {
@@ -43,7 +47,51 @@
       }
       return false;
     }
-     ?>
+    // print_r(get_product(1));
+
+    // add all data to array
+    $test_Array = [];
+    for ($i=0; $i< count(read_all_products()) + 1; $i++) {
+      $test_Array[] = get_product($i);
+    }
+
+    echo '<pre> ';
+    var_dump($test_Array)."<br>";
+    echo '</pre> ';
+
+    // echo count($test_Array);
+
+    $test_length = count($test_Array);
+
+
+    // for ($i=0; $i <$test_length; $i++) {
+    //     echo '<pre> ';
+    //     var_dump($test_Array[$i]['created_time'])."<br>";
+    //     echo '</pre> ';
+    // }
+
+    $valid_product_array = [];
+    for ($i=0; $i <$test_length ; $i++) {
+      if ($test_Array[$i]['featured_in_mall'] != 'FALSE' && $valid_product_array[$i] != $test_Array[$i] ) {
+          $valid_product_array[] = $test_Array[$i];
+      }
+    };
+
+    // echo count($valid_product_array);
+    // for ($i=0; $i <count($valid_product_array); $i++) {
+    //     echo '<pre> ';
+    //     var_dump($valid_product_array[$i])."<br>";
+    //     echo '</pre> ';
+    // }
+
+
+
+    // echo '<pre> ';
+    // var_dump($valid_product_array)."<br>";
+    // echo '</pre> ';
+
+
+    ?>
 
   </body>
 </html>
